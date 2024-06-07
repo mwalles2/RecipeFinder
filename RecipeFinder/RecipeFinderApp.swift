4a34b5c062bd30f6d2247c1d726c962520f8b7ac
Micah A. Walles <micah@walles.info>
2024-06-07 14:39:35 -0500

    Initial Commit

---
+++ RecipeFinder/RecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderApp.swift
+//  RecipeFinder
+//
+//  Created by Micah A. Walles on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
